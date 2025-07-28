aa81ce386b6e64a213a4c3365d4d1b8e18ec155b
yi0709-fgo <15071248053@163.com>
2025-07-28 23:03:34 +0200

    improve name

--- src/main/java/com/example/service/MostActiveCookie.java
+++ src/main/java/com/example/service/MostActiveCookie.java
@@ -2,6 +2,9 @@ package com.example.service;
 
 import com.example.domain.LogData;
 import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneOffset;
 import java.util.*;
 
 import static com.example.parse.ParseFile.parseLog;
@@ -14,6 +17,7 @@ public class MostActiveCookie {
         List<LogData> logDataList = parseLog(filePath);
         Map<String, Integer> counter = new HashMap<>();
         for(LogData logData:logDataList){
+
             String date = logData.getTimeStamp().split("T")[0];
             if(date.equals(targetDate)){
                 counter.put(logData.getCookie(),counter.getOrDefault(logData.getCookie(), 0) + 1);
@@ -29,6 +33,39 @@ public class MostActiveCookie {
                 .map(Map.Entry::getKey)
                 .toList();
 
+
+        return mostActiveCookies;
+    }
+
+
+    public static List<String> process(String filePath, String targetDate, int topN) throws IOException {
+        List<String> mostActiveCookies = new ArrayList<>();
+        List<LogData> logDataList = parseLog(filePath);
+        Map<String, Integer> counter = new HashMap<>();
+        for(LogData logData:logDataList){
+//            LocalDate logDate = Instant.parse(logData.getTimeStamp())
+//                    .atZone(ZoneOffset.UTC)
+//                    .toLocalDate();
+            String date = logData.getTimeStamp().split("T")[0];
+            if(date.equals(targetDate)){
+                counter.put(logData.getCookie(),counter.getOrDefault(logData.getCookie(), 0) + 1);
+            }
+        }
+        if(counter.isEmpty()){
+            return mostActiveCookies;
+        }
+        List<Map.Entry<String, Integer>> sorted = new ArrayList<>(counter.entrySet());
+        sorted.sort((a, b) -> b.getValue().compareTo(a.getValue()));
+        int rank = 0;
+        int preCount = -1;
+        for(Map.Entry<String, Integer> entry:sorted){
+            if(rank>=topN && preCount<entry.getValue()) break;
+            mostActiveCookies.add(entry.getKey());
+            if(preCount!=entry.getValue()){
+                rank++;
+                preCount = entry.getValue();
+            }
+        }
         return mostActiveCookies;
     }
 }
